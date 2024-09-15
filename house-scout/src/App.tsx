2a97b319516552831f3acafeaa237e1b4ab6a77a
Luk1e <luka.gogiashvili.02@gmail.com>
2024-09-15 21:51:54 +0400

    feat: added router to app component N2

--- house-scout/src/App.tsx
+++ house-scout/src/App.tsx
@@ -1,14 +1,11 @@
-import { Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./routes/routes";
 
 function App() {
   return (
-    <Suspense fallback={null}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </Suspense>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   );
 }
 
