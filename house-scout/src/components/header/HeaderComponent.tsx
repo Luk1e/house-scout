f7bc8ea08b32cfbfdebfaa1b46e567c80386e6ac
Luk1e <luka.gogiashvili.02@gmail.com>
2024-10-30 22:39:01 +0400

    fix: changed header logo

--- house-scout/src/components/header/HeaderComponent.tsx
+++ house-scout/src/components/header/HeaderComponent.tsx
@@ -1,4 +1,3 @@
-import { logo } from "../../assets/images";
 import { useNavigate } from "react-router-dom";
 
 function HeaderComponent() {
@@ -6,12 +5,9 @@ function HeaderComponent() {
 
   return (
     <header className="box-content; border border-[#DBDBDB] w-full px-10 py-5 md:px-15 md:py-10 lg:px-28 lg:py-15 2xl:px-[162px] 2xl:py-[38px]">
-      <img
-        src={logo}
-        alt="Redberry logo"
-        className="w-[150px] h-[24px] hover:cursor-pointer"
-        onClick={() => navigate("/")}
-      />
+      <h1 onClick={() => navigate("/")} className="text-[#F93B1D] text-4xl font-bold cursor-pointer">
+        House Scout
+      </h1>
     </header>
   );
 }
